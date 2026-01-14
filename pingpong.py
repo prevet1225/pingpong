015ba9d52410c07a280b2bd9c605453d2fb57eab
prevet1225 <prevet458@gmail.com>
2026-01-14 18:16:12 +0300

    player+win+ball
    v0.2

--- pingpong.py
+++ pingpong.py
@@ -1,11 +1,11 @@
 from pygame import *
 
 win = display.set_mode((1080 , 720))
-clock = time.Clock
+clock = time.Clock()
 
 class Player():
     def __init__(self,x,y,speed):
-        self.hitbox = Rect(x,y,50,150)
+        self.hitbox = Rect(x,y,30,150)
         self.speed = speed
     def move(self):
         key_list = key.get_pressed()
@@ -18,7 +18,26 @@ class Player():
         if self.hitbox.top < 0:
             self.hitbox.top = 0
 
-player_1 = Player(50,120,2)
+class Ball():
+    def __init__(self,x,y,speed):
+        self.hitbox = Rect(x,y,34,34)
+        self.speed = speed
+        self.speed_x = speed
+        self.speed_y = speed
+    def move(self):
+        self.hitbox.x += self.speed_x
+        self.hitbox.y += self.speed_y
+        if self.hitbox.top < 0:
+            self.speed_y = self.speed
+        if self.hitbox.bottom > 720:
+            self.speed_y = -self.speed
+        if self.hitbox.left < 0:
+            self.speed_x = self.speed
+        if self.hitbox.right > 1080:
+            self.speed_x = -self.speed
+
+player_1 = Player(50,240,2)
+ball = Ball(540,360,3)
 
 while True:
     win.fill((0,0,0))
@@ -28,4 +47,7 @@ while True:
             exit()
     draw.rect(win,(255,255,255),player_1.hitbox)
     player_1.move()
+    draw.rect(win,(255,255,255),ball.hitbox)
+    ball.move()
+    clock.tick(60)
     display.update()
